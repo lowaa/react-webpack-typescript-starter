3328a48f75f28a88f08f98d06642e3be3c7cee08
aaron <aaron.low.au@gmail.com>
2021-12-19 08:26:27 +0800

    Lock and formatting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,15 +5,15 @@ const reactLogo = require("./../assets/img/react_logo.svg");
 import "./../assets/scss/App.scss";
 
 class App extends React.Component<Record<string, unknown>, undefined> {
-  public render() {
-    return (
-      <div className="app">
-        <h1>Hello World!</h1>
-        <p>Foo to the barz</p>
-        <img src={reactLogo.default} height="480" />
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div className="app">
+                <h1>Hello World!</h1>
+                <p>Foo to the barz</p>
+                <img src={reactLogo.default} height="480" />
+            </div>
+        );
+    }
 }
 
 declare let module: Record<string, unknown>;
